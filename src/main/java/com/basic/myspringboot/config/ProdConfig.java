2bef527ac7d1194df9eb4a086b288ca6d16cc315
mysoyul <vega2k_@naver.com>
2024-11-14 15:17:56 +0900

    @Profile use

--- src/main/java/com/basic/myspringboot/config/ProdConfig.java
+++ src/main/java/com/basic/myspringboot/config/ProdConfig.java
@@ -3,7 +3,9 @@ package com.basic.myspringboot.config;
 import com.basic.myspringboot.config.vo.CustomVO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("prod")
 @Configuration
 public class ProdConfig {
     @Bean
