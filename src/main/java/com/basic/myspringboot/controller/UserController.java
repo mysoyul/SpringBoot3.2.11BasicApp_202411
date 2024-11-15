daeeb593285046fd1ed74a6a8f96ffbd8963707d
mysoyul <vega2k_@naver.com>
2024-11-15 15:36:12 +0900

    UserController update

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -1,14 +1,26 @@
 package com.basic.myspringboot.controller;
 
+import com.basic.myspringboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
         model.addAttribute("name", "스프링부트");
         return "leaf";
     }
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+    }
+
 }
