82069982eb9c3821c5ee1d716aa75228eb04ceaa
mysoyul <vega2k_@naver.com>
2024-11-15 11:15:00 +0900

    CustomerRepositoryTest findByCustomerId

--- src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -4,15 +4,23 @@ import com.basic.myspringboot.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class CustomerRepositoryTest {
     @Autowired
     CustomerRepository repository;
 
     @Test
+    //Rollback 처리 하지 않기
+    @Rollback(value = false)
     void insert() {
         //Entity 객체생성
         Customer customer = new Customer();
@@ -21,6 +29,22 @@ class CustomerRepositoryTest {
 
         //CrudRepository 의 save() 호출
         Customer savedCustomer = repository.save(customer);
+
+        assertThat(savedCustomer).isNotNull();
+        assertThat(savedCustomer.getCustomerName()).isEqualTo("스프링");
+        assertEquals("A001",savedCustomer.getCustomerId());
+
+        //PK로 조회하기
+        Optional<Customer> byId = repository.findById(1L);
+        if(byId.isPresent()){
+            Customer existCustomer = byId.get();
+            assertThat(existCustomer.getCustomerName()).isEqualTo("스프링");
+        }
+
+        //CustomerId로 조회하기
+        Customer customer1 = repository.findByCustomerId("A001")
+                .orElseThrow(() -> new RuntimeException("Customer Not Found"));
+        assertThat(customer1.getCustomerName()).isEqualTo("스프링");
 
 
     }
