40a9e29f35d35717c6305d5246c6c458f1855e3b
mysoyul <vega2k_@naver.com>
2024-11-15 13:50:18 +0900

    DefaultExceptionAdvice add

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -1,13 +1,12 @@
 package com.basic.myspringboot.controller;
 
 import com.basic.myspringboot.entity.UserEntity;
+import com.basic.myspringboot.exception.BusinessException;
 import com.basic.myspringboot.repository.CustomerRepository;
 import com.basic.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +23,14 @@ public class UserRestController {
     public UserEntity create(@RequestBody UserEntity user) {
         return userRepository.save(user);
     }
+
+    @GetMapping("/{id}")
+    public UserEntity getUserById(@PathVariable Long id) {
+        return userRepository.findById(id) //Optional<UserEntity>
+                //orElseThrow(Supplier) Supplier 추상메서드 T get()
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+    }
+
 
 
 }
