155f381c668a765a3362043f8d87cbb776ac41f4
mysoyul <vega2k_@naver.com>
2024-11-15 11:28:11 +0900

    @DynamicUpdate

--- src/main/java/com/basic/myspringboot/entity/Customer.java
+++ src/main/java/com/basic/myspringboot/entity/Customer.java
@@ -3,11 +3,13 @@ package com.basic.myspringboot.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
 @Table(name = "customers")
 @Getter
 @Setter
+@DynamicUpdate
 public class Customer {
     //Primary Key
     @Id  
