e1f9d8b9e63f0df18e6cdf0ad791372d43f9bd1f
mysoyul <vega2k_@naver.com>
2024-11-15 13:11:36 +0900

    User Entity Repository update

--- src/main/java/com/basic/myspringboot/repository/UserRepository.java
+++ src/main/java/com/basic/myspringboot/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.basic.myspringboot.repository;
 import com.basic.myspringboot.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByName(String name);
+    List<User> findByName(String name);
+    Optional<User> findByEmail(String email);
 }
